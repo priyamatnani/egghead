192f17f13f982b0b8995da7ec207dc87067d6c48
priya.matnani <priya.matnani@sla-ubu-381.punesez.searce.local>
2014-07-14 16:42:41 +0530

    video 4

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
-function FirstCtrl($scope) {
-    $scope.data = { name : "priya"}
+var myApp = angular.module('myApp',[]);
+myApp.factory('Data', function(){
+    return { name: "I am data from a service" }
+ })
+
+
+function FirstCtrl($scope, Data) {
+    $scope.data = Data;
+}
+
+function SecondCtrl($scope,Data){
+
+    $scope.data=Data;
 }
