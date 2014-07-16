7fd35913ae1bfee8b27f4debf98a89186d1d8bc8
priya.matnani <priya.matnani@sla-ubu-381.punesez.searce.local>
2014-07-16 09:24:28 +0530

    controller

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 var myApp = angular.module('myApp',[]);
 myApp.factory('Data', function(){
-    return { name: "I am data from a service" }
+    return { message: "I am data from a service" }
  })
 
+
+myApp.filter('reverse' , function(){
+
+    return function(text){
+
+        return text.split("").reverse().join("");
+    }
+})
 
 function FirstCtrl($scope, Data) {
     $scope.data = Data;
@@ -11,4 +19,9 @@ function FirstCtrl($scope, Data) {
 function SecondCtrl($scope,Data){
 
     $scope.data=Data;
+
+   /* $scope.reversedMessage = function(message){
+
+        return message.split("").reverse().join("");
+    }*/
 }
